b2d97f7d0e7617e06901a5eccecdb6899247eb3d
KID1412-myc <3061610547@qq.com>
2025-12-09 11:34:33 +0800

    页面重绘

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ from werkzeug.utils import secure_filename
 load_dotenv()
 
 app = Flask(__name__)
-CURRENT_APP_VERSION = '2.7.1'
+CURRENT_APP_VERSION = '3.0.0'
 qweather_key = os.environ.get("QWEATHER_KEY")
 qweather_host = os.environ.get("QWEATHER_HOST", "https://devapi.qweather.com")
 ENABLE_GOD_MODE = False
